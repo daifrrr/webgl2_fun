2cee2945079cf0513272096a04ef988f11fdb451
zschornak <d.zschornak@queo-group.com>
2019-01-31 10:50:01 +0100

    inc Camera

--- src/js/index.js
+++ src/js/index.js
@@ -6,43 +6,38 @@ import RenderLoop from './RenderLoop';
 import Shader from './Shader';
 import Modal from './Modal';
 import Primitives from './Primitives';
-import * as glm from 'gl-matrix';
+import {Camera, CameraController} from "./Camera";
 
 let gl,
     gRLoop,
     gModal,
-    gShader;
+    gShader,
+    gCamera,
+    gCameraControl;
 
 window.addEventListener('load', function () {
     gl = GLInstance('glCanvas').fSetSize(500, 500).fClear();
 
-    gShader = new TestShader(gl, [0.8, 0.8, 0.8,
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, 1]);
+    gCamera = new Camera(gl);
+    gCamera.transform.position.set(0,1,3);
+    gCameraControl = new CameraController(gl, gCamera);
 
+
+    gShader = new TestShader(gl, [0.8, 0.8, 0.8,  1, 0, 0,  0, 1, 0,  0, 0, 1]);
+    gShader.activate().setPerspective(gCamera.projectionMatrix).deactivate();
 
     gModal = new Modal(Primitives.GridAxis.createMesh(gl))
-        .setScale(0.4, 0.4, 0.4)
-        .setRotation(0, 0, 45)
-        .setPosition(0.8, 0.8, 0);
-    console.log(gModal);
+        .setScale(2, 2, 0)
+        .setPosition(0,0,-1);
     gRLoop = new RenderLoop(onRender).start();
 });
 
 function onRender(dt) {
+    gCamera.updateViewMatrix();
     gl.fClear();
-    console.log(dt);
-    let p = gModal.transform.position,
-        angle = Math.atan2(p.y, p.x) + (1 * dt),
-        radius = Math.sqrt(p.x * p.x + p.y * p.y),
-        scale = Math.max(0.2, Math.abs(Math.sin(angle)) * 1.2);
-    gShader.activate().renderModal(
-        gModal.setScale(scale, scale / 4, 1)
-            .setPosition(radius * Math.cos(angle), radius * Math.sin(angle), 0)
-            .addRotation(30 * dt, 60 * dt, 15 * dt)
-            .preRender()
-    );
+    gShader.activate()
+        .setCameraMatrix(gCamera.viewMatrix)
+        .renderModal(gModal.preRender());
 }
 
 class TestShader extends Shader {
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -2,12 +2,15 @@
 in vec3 a_position;
 layout(location=4) in float a_color;
 
+uniform mat4 uPMatrix;
 uniform mat4 uMVMatrix;
+uniform mat4 uCameraMatrix;
+
 uniform vec3 uColor[4];
 
 out lowp vec4 color;
 
 void main() {
     color = vec4(uColor[int(a_color)], 1.0);
-    gl_Position = uMVMatrix * vec4(a_position, 1.0);
+    gl_Position = uPMatrix * uCameraMatrix * uMVMatrix * vec4(a_position, 1.0);
 }
